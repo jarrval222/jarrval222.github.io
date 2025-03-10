b9f0449e96f9002bb14596c96938cd27232263a8
jarrval222 <jarrval222@g.educaand.es>
2025-03-10 22:25:38 +0100

    cambios en acciones y estilos más dinamicos

---
+++ stars.js
@@ -0,0 +1,11 @@
+document.addEventListener('mousemove', function(e) {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    star.style.left = `${e.pageX}px`;
+    star.style.top = `${e.pageY}px`;
+    document.body.appendChild(star);
+
+    setTimeout(() => {
+        star.remove();
+    }, 1000);
+});
